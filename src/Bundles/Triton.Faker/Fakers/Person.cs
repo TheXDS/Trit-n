6b883aaab1e4235cb72aa35f0f911e0dd945fb5a
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2019-11-27 19:42:10 -0600

    Bundles de servicios externos

---
+++ src/Bundles/Triton.Faker/Fakers/Person.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+using TheXDS.MCART.Types.Extensions;
+using TheXDS.Triton.Resources;
+using static TheXDS.Triton.Fakers.Globals;
+
+namespace TheXDS.Triton.Fakers
+{
+    public class Person
+    {
+        private string _userName;
+
+        public string FirstName { get; }
+        public string Surname { get; }
+        public Gender Gender { get; }
+        public DateTime Birth { get; }
+        public string UserName => _userName ??= FakeUsername(this);
+        public string Name => string.Join(' ', FirstName, Surname);
+        public string FullName => string.Join(", ", Surname, FirstName);
+        public double Age => (DateTime.Today - Birth).TotalDays / 365.25;
+
+        private Person(string firstName, string surname, Gender gender, DateTime birth)
+        {
+            FirstName = firstName;
+            Surname = surname;
+            Gender = gender;
+            Birth = birth;
+        }
+
+        public static Person Adult()
+        {
+            return Someone(18, 60);
+        }
+
+        public static Person Kid()
+        {
+            return Someone(5, 18);
+        }
+
+        public static Person Baby()
+        {
+            return Someone(0, 5);
+        }
+
+        public static Person Old()
+        {
+            return Someone(60, 110);
+        }
+
+        public static Person Someone(int minAge, int maxAge)
+        {
+            var m = _rnd.CoinFlip();
+
+            return new Person(
+                (m ? StringTables.MaleNames : StringTables.FemaleNames).Pick(),
+                StringTables.Surnames.Pick(),
+                m ? Gender.Male : Gender.Female,
+                FakeBirth(minAge, maxAge));
+        }
+
+        public static Person Someone()
+        {
+            return new Func<Person>[] {Baby, Kid, Adult, Old }.Pick().Invoke();
+        }
+
+        public static DateTime FakeBirth(int minAge, int maxAge)
+        {
+            var a = _rnd.NextDouble();
+            return (DateTime.Today - TimeSpan.FromDays(((a * (maxAge - minAge)) + minAge)*365.25)).Date;
+        }
+
+        public static string FakeUsername(Person person)
+        {
+            var sb = new StringBuilder();
+            var rounds = _rnd.Next(1, 4);
+            sb.Append(new[] { person.Surname, person.FirstName, StringTables.Lorem.Pick() }.Pick());
+
+            do
+            {
+                if (_rnd.CoinFlip()) sb.Append('_');
+                sb.Append(new[] { StringTables.Lorem.Pick(), person.Birth.Year.ToString(), person.Birth.Year.ToString().Substring(2), _rnd.Next(0, 1000).ToString().PadLeft(_rnd.Next(1, 4), '0') }.Pick());
+            } while (--rounds > 0);
+
+            return sb.ToString().ToLower();
+
+        }
+
+        public static string FakeUsername()
+        {
+            var sb = new StringBuilder();
+            var rounds = _rnd.Next(1, 4);
+
+            sb.Append(StringTables.Lorem.Pick());
+            do
+            {
+                if (_rnd.CoinFlip()) sb.Append('_');
+                sb.Append(new[] { StringTables.Lorem.Pick(), _rnd.Next(0, 10000).ToString().PadLeft(_rnd.Next(1, 5), '0') }.Pick());
+            } while (--rounds > 0);
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ src/Bundles/Triton.Faker/Fakers/Text.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using TheXDS.MCART.Types.Extensions;
+using TheXDS.Triton.Resources;
+using static TheXDS.Triton.Fakers.Globals;
+
+namespace TheXDS.Triton.Fakers
+{
+    public static class Text
+    {
+
+        public static string Lorem(in int words)
+        {
+            const double delta = 0.3; // Delta de variación, en %
+            const double wps = 6;      // Palabras por oración
+            const double spp = 5;      // Oraciones por párrafo.
+
+            var text = new StringBuilder();
+            
+            var wc = 0; // Cuenta de palabras.
+            do
+            {
+                text.Append(StringTables.Lorem.Pick());
+                wc++;
+                var wpts = Variate(wps, delta);
+                var swc = 0;
+                do
+                {
+                    if (wc == words) break;
+
+
+
+                } while (swc < wps);
+                text.AppendLine(".");
+            } while (wc < words);
+
+            return text.ToString();
+        }
+
+        private static string Capitalize(string value)
+        {
+            return value.Substring(0, 1).ToUpper() + value.Substring(1).ToLower();
+        }
+
+        private static double Variate(in double value, in double delta)
+        {
+            var d = value * delta;
+            var d2 = (d * 2) - d;
+            return value + _rnd.NextDouble() * d2;
+        }
+    }
+}
