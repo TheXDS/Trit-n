4a9acc83f69bc1940c98da9f0537491d76ee7766
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2019-12-05 23:46:08 -0600

    WIP: servicio de notificación

---
+++ src/Bundles/Triton.CrudNotify/ICrudNotifier.cs
@@ -0,0 +1,27 @@
+﻿using TheXDS.Triton.Models.Base;
+using TheXDS.Triton.Services;
+
+namespace TheXDS.Triton.Middleware
+{
+    /// <summary>
+    ///     Define una serie de miembros a implementar por un tipo que envíe
+    ///     notificaciones de eventos Crud a otros equipos conectados.
+    /// </summary>
+    public interface ICrudNotifier
+    {
+        /// <summary>
+        ///     Envía una notificación de un evento Crud a todos los equipos
+        ///     conectados.
+        /// </summary>
+        /// <param name="action">
+        ///     Acción Crud que se ha realizado.
+        /// </param>
+        /// <param name="entity">
+        ///     Entidad sobre la cual se ha realizado una operación Crud.
+        /// </param>
+        /// <returns>
+        ///     El resultado de una operación de servicio.
+        /// </returns>
+        ServiceResult NotifyPeers(CrudAction action, Model? entity);
+    }
+}
---
+++ src/Extras/RelayBaron/RelayBaron.Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using TheXDS.MCART.Networking.Server;
+using TheXDS.Triton.Services;
+using static TheXDS.Triton.Services.CrudAction;
+
+namespace RelayBaron.Server
+{
+    internal static class Program
+    {
+        static Server<Client> _srv = new Server<Client>(new RelayBaronProtocol(), 61440);
+        static void Main(string[] args)
+        {
+            _srv.Start();
+            Console.CancelKeyPress += OnExit;
+            while (_srv.IsAlive)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        private static void OnExit(object sender, ConsoleCancelEventArgs e)
+        {
+            _srv.Stop();   
+            Environment.Exit(0);
+        }
+    }
+
+    internal class RelayBaronProtocol : ManagedCommandProtocol<Client, CrudAction, CrudAction>
+    {
+        static RelayBaronProtocol()
+        {
+            ScanTypeOnCtor = false;
+        }
+
+        public RelayBaronProtocol()
+        {
+            WireUp(Create, Relay);
+            WireUp(Read, Relay);
+            WireUp(Update, Relay);
+            WireUp(Delete, Relay);
+        }
+
+        private void Relay(Request request)
+        {
+            request.Respond(Commit);
+            request.Broadcast(request.Command, request.Reader.BaseStream);
+        }
+    }
+}
