697262837d4e26cb3092e3a0c98d71526626964b
César Morgan <xds_xps_ivx@hotmail.com>
2020-07-19 11:22:03 -0600

    WIP: nuevo cliente para Cocoa

---
+++ src/Clients/Cocoa/Triton.Workstation.CocoaClient/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Triton.Workstation.CocoaClient
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
