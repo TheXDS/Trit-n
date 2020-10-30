dd06a8fe83aec98ba74591bfa6b8b60bdb3047c9
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-10-30 10:10:26 -0600

    Documentación

--- src/Transport/Triton.InMemory/Services/InMemoryCrudTransaction.cs
+++ src/Transport/Triton.InMemory/Services/InMemoryCrudTransaction.cs
@@ -11,19 +11,47 @@ using TheXDS.Triton.Services.Base;
 
 namespace TheXDS.Triton.InMemory.Services
 {
+    /// <summary>
+    /// Representa una transacción de prueba que almacena los datos guardados
+    /// en la memoria de la aplicación. Los datos almacenados no se persistirán
+    /// y serán borrados al finalizar la ejecución.
+    /// </summary>
     public class InMemoryCrudTransaction : AsyncDisposable, ICrudReadWriteTransaction
     {
         private static readonly List<Model> Models = new List<Model>();
 
         private readonly List<Model> _temp = new List<Model>();
 
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase
+        /// <see cref="InMemoryCrudTransaction"/>.
+        /// </summary>
+        /// <param name="configuration">
+        /// Configuración de transacciones a utilizar.
+        /// </param>
         public InMemoryCrudTransaction(TransactionConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Obtiene la configuración de transacciones que ha sido establecida
+        /// en esta transacción.
+        /// </summary>
         public TransactionConfiguration Configuration { get; }
 
+        /// <summary>
+        /// Obtiene un <see cref="ServiceResult"/> con un Query de todas las
+        /// entidades de la base de datos que corresponden a un modelo
+        /// específico.
+        /// </summary>
+        /// <typeparam name="TModel">
+        /// Modelo a obtener desde la base de datos.
+        /// </typeparam>
+        /// <returns>
+        /// Un <see cref="IQueryable{TModel}"/> que representa el Query de
+        /// todas las entidades del modelo especificado desde la base de datos.
+        /// </returns>
         public QueryServiceResult<TModel> All<TModel>() where TModel : Model
         {
             lock (((ICollection)Models).SyncRoot)
@@ -32,6 +60,10 @@ namespace TheXDS.Triton.InMemory.Services
             }
         }
         
+        /// <summary>
+        /// Guarda los cambios en la base de datos de forma asíncrona.
+        /// </summary>
+        /// <returns></returns>
         public Task<ServiceResult> CommitAsync()
         {
             Models.AddRange(_temp.Where(p => !Models.Contains(p)));
@@ -39,6 +71,17 @@ namespace TheXDS.Triton.InMemory.Services
             return Task.FromResult(ServiceResult.Ok);
         }
 
+        /// <summary>
+        /// Crea una nueva entidad en la base de datos.
+        /// </summary>
+        /// <typeparam name="TModel">Modelo de la nueva entidad.</typeparam>
+        /// <param name="newEntity">
+        /// Nueva entidad a agregar a la base de datos.
+        /// </param>
+        /// <returns>
+        /// Un <see cref="ServiceResult"/> que representa el éxito o fracaso de
+        /// la operación.
+        /// </returns>
         public ServiceResult Create<TModel>(TModel newEntity) where TModel : Model
         {
             if (Models.Concat(_temp).Contains(newEntity)) return FailureReason.EntityDuplication;
@@ -46,11 +89,36 @@ namespace TheXDS.Triton.InMemory.Services
             return ServiceResult.Ok;
         }
 
+        /// <summary>
+        /// Elimina una entidad de la base de datos.
+        /// </summary>
+        /// <typeparam name="TModel">Modelo de la entidad.</typeparam>
+        /// <param name="entity">
+        /// Entidad a eliminar de la base de datos.
+        /// </param>
+        /// <returns>
+        /// Un <see cref="ServiceResult"/> que representa el éxito o fracaso de
+        /// la operación.
+        /// </returns>
         public ServiceResult Delete<TModel>(TModel entity) where TModel : Model
         {
             return !Models.Remove(entity) || _temp.Remove(entity) ? new ServiceResult(FailureReason.NotFound) : ServiceResult.Ok;
         }
 
+        /// <summary>
+        /// Elimina una entidad de la base de datos.
+        /// </summary>
+        /// <typeparam name="TModel">Modelo de la entidad.</typeparam>
+        /// <typeparam name="TKey">
+        /// Tipo del campo llave utilizado para identificar a la entidad.
+        /// </typeparam>
+        /// <param name="key">
+        /// Valor del campo llave utilizado para identificar a la entidad.
+        /// </param>
+        /// <returns>
+        /// Un <see cref="ServiceResult"/> que representa el éxito o fracaso de
+        /// la operación.
+        /// </returns>
         public ServiceResult Delete<TModel, TKey>(TKey key)
             where TModel : Model<TKey>
             where TKey : IComparable<TKey>, IEquatable<TKey>
@@ -60,6 +128,20 @@ namespace TheXDS.Triton.InMemory.Services
                 : new ServiceResult(FailureReason.NotFound);
         }
 
+        /// <summary>
+        /// Lee una entidad desde la base de datos.
+        /// </summary>
+        /// <typeparam name="TModel">Modelo de la entidad.</typeparam>
+        /// <typeparam name="TKey">
+        /// Tipo del campo llave utilizado para identificar a la entidad.
+        /// </typeparam>
+        /// <param name="key">
+        /// Valor del campo llave utilizado para identificar a la entidad.
+        /// </param>
+        /// <returns>
+        /// Un <see cref="ServiceResult"/> que representa el éxito o fracaso de
+        /// la operación, incluyendo a la entidad obtenida.
+        /// </returns>
         public Task<ServiceResult<TModel?>> ReadAsync<TModel, TKey>(TKey key)
             where TModel : Model<TKey>
             where TKey : notnull, IComparable<TKey>, IEquatable<TKey>
@@ -69,16 +151,39 @@ namespace TheXDS.Triton.InMemory.Services
                 : new ServiceResult<TModel?>(FailureReason.NotFound));
         }
 
+        /// <summary>
+        /// Actualiza los datos de una entidad existente.
+        /// </summary>
+        /// <typeparam name="TModel">Modelo de la entidad.</typeparam>
+        /// <param name="entity">
+        /// Entidad con los nuevos datos a ser escritos. Debe existir en la
+        /// base de datos una entidad con el mismo Id de este objeto.
+        /// </param>
+        /// <returns>
+        /// Un <see cref="ServiceResult"/> que representa el éxito o fracaso de
+        /// la operación.
+        /// </returns>
         public ServiceResult Update<TModel>(TModel entity) where TModel : Model
         {
             return Models.Concat(_temp).Contains(entity) ? ServiceResult.Ok : new ServiceResult(FailureReason.NotFound);
         }
 
+        /// <summary>
+        /// Libera los recursos desechables utilizados por esta instancia.
+        /// </summary>
         protected override void OnDispose()
         {
             if (_temp.Any()) CommitAsync().ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// Libera de forma asíncrona los recursos desechables utilizados por
+        /// esta instancia.
+        /// </summary>
+        /// <returns>
+        /// Un objeto que puede ser utilizado para monitorear el estado de la
+        /// tarea.
+        /// </returns>
         protected override async ValueTask OnDisposeAsync()
         {
             if (_temp.Any()) await CommitAsync().ConfigureAwait(false);
--- src/Transport/Triton.InMemory/Services/InMemoryTransFactory.cs
+++ src/Transport/Triton.InMemory/Services/InMemoryTransFactory.cs
@@ -3,8 +3,23 @@ using TheXDS.Triton.Services.Base;
 
 namespace TheXDS.Triton.InMemory.Services
 {
+    /// <summary>
+    /// Fábrica de transacciones que genera transacciones sin persistencia (en
+    /// memoria).
+    /// </summary>
     public class InMemoryTransFactory : ITransactionFactory
     {
+        /// <summary>
+        /// Fabrica una transaccion conectada a un almacén volátil sin
+        /// persistencia en la memoria de la aplicación.
+        /// </summary>
+        /// <param name="configuration">
+        /// Configuración de transacción a utilizar.
+        /// </param>
+        /// <returns>
+        /// Una transacción conectada a un almacén volátil sin persistencia en
+        /// la memoria de la aplicación.
+        /// </returns>
         public ICrudReadWriteTransaction GetTransaction(TransactionConfiguration configuration)
         {
             return new InMemoryCrudTransaction(configuration);
