58aee2eee8fb04be97993d351263cf8cdab0ec9f
César Morgan <xds_xps_ivx@hotmail.com>
2022-03-15 14:40:26 -0600

    Basic model tests

---
+++ src/Tests/Triton.Tests/Tests/BasicModelTests.cs
@@ -0,0 +1,30 @@
+﻿#pragma warning disable CS1591
+
+using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using TheXDS.MCART.Types.Extensions;
+using TheXDS.Triton.Models.Base;
+
+namespace TheXDS.Triton.Tests
+{
+    public class BasicModelTests
+    {
+        private class ConcurrentTestModel : ConcurrentModel<int>
+        {
+        }
+
+        [Test]
+        public void ConcurrentModel_T_includes_RowVersion()
+        {
+            var t = typeof(ConcurrentTestModel);
+            Assert.NotNull(t.GetProperties().SingleOrDefault(p => p.IsReadWrite() && p.PropertyType == typeof(byte[]) && p.HasAttr<TimestampAttribute>()));
+            var x = new ConcurrentTestModel();
+            Assert.AreEqual(default(bool[]), x.RowVersion);
+            var a = RandomNumberGeneratorExtensions.GetBytes(16);
+            x.RowVersion = a;
+            Assert.AreEqual(a, x.RowVersion);
+
+        }
+    }
+}
