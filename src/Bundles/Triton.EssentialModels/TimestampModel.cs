8343f2a753c51d228d70812593d4fa02ddded277
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2022-04-20 20:07:40 -0600

    WIP: Bundle de modelos útiles

---
+++ src/Bundles/Triton.EssentialModels/TimestampModel.cs
@@ -0,0 +1,38 @@
+namespace TheXDS.Triton.Models;
+using System;
+using TheXDS.Triton.Models.Base;
+
+/// <summary>
+/// Modelo base para aquellas entidades que expongan campos de marca de tiempo.
+/// </summary>
+/// <typeparam name="T">
+/// Tipo de campo llave a utilizar para este modelo.
+/// </typeparam>
+public abstract class TimestampModel<T> : Model<T> where T : notnull, IComparable<T>, IEquatable<T>
+{
+    /// <summary>
+    /// Obtiene o establece una marca de tiempo a asociar con esta entidad.
+    /// </summary>
+    /// <value></value>
+    public DateTime Timestamp { get; set; }
+
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase
+    /// <see cref="TimestampModel{T}"/>.
+    /// </summary>
+    /// <param name="timestamp">
+    /// Marca de tiempo a asociar a esta entidad.
+    /// </param>
+    public TimestampModel(DateTime timestamp)
+    {
+        Timestamp = timestamp;
+    }
+
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase
+    /// <see cref="TimestampModel{T}"/>.
+    /// </summary>
+    public TimestampModel()
+    {
+    }
+}
