0664d7fb0dfcd12c6155b4c5598632a747859400
César Morgan <xds_xps_ivx@hotmail.com>
2022-06-19 00:40:12 -0600

    Cleanup

---
+++ src/Tests/Bundles/Triton.Tests.Faker/CompanyTests.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable CS1591
+
+using NUnit.Framework;
+using System;
+using System.Linq;
+using TheXDS.Triton.Faker;
+
+namespace TheXDS.Triton.Tests.Faker;
+
+public class CompanyTests
+{
+    [Test]
+    public void Company_has_fake_data()
+    {
+        foreach (var _ in Enumerable.Range(0, 100))
+        {
+            Company c = new();
+            Assert.IsNotNull(c);
+            Assert.IsNotEmpty(c.Name);
+            Assert.IsNotNull(c.Address);
+            Assert.IsNotEmpty(c.DomainName);
+            Assert.IsNotEmpty(c.Website);
+            Assert.IsTrue(Uri.IsWellFormedUriString(c.Website, UriKind.Absolute));
+        }
+    }
+}
